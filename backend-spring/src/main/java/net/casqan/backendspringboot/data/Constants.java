479510e44e802a0dd95ab18117441dec3ec2b1a3
casqan.edu <Marc.Matija@student.hs-rm.de>
2024-08-22 09:31:31 +0200

    [k6] Added Message Tests

--- backend-spring/src/main/java/net/casqan/backendspringboot/data/Constants.java
+++ backend-spring/src/main/java/net/casqan/backendspringboot/data/Constants.java
@@ -3,7 +3,7 @@ package net.casqan.backendspringboot.data;
 public final class Constants {
     public static final String API_VERSION = "v1";
     public static final String API_BASE_URL = "/api/" + API_VERSION + "/";
-    public static final String PROFILE_BASE_URL = API_BASE_URL + "profile/";
-    public static final String CHANNEL_BASE_URL = API_BASE_URL + "channel/";
-    public static final String MESSAGE_BASE_URL = CHANNEL_BASE_URL + "{channelId}/message/";
+    public static final String PROFILE_BASE_URL = API_BASE_URL + "profiles/";
+    public static final String CHANNEL_BASE_URL = API_BASE_URL + "channels/";
+    public static final String MESSAGE_BASE_URL = CHANNEL_BASE_URL + "{channelId}/messages/";
 }
